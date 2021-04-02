e9fa1aa316e670363b7eb3aead5f85ab3f7f6eee
Michael Bilodeau <michael.bilodeau@optelgroup.com>
2021-04-02 08:40:14 -0400

    Add first code

---
+++ amqpcpp-test/main.cpp
@@ -0,0 +1,11 @@
+﻿
+#include <string>
+#include <iostream>
+#include <optional>
+
+int main()
+{ 
+    std::optional<std::string> str = "Hello world\n";
+    std::cout << str.value_or("No hello\n");
+    return 0;
+}
